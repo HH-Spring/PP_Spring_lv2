dc04d2835eef17e65eb57af22e1bae22fe847e05
muyeon <gmakin36@gmail.com>
2024-02-26 21:27:17 +0900

    init

---
+++ src/main/java/org/sparta/hh_lv2/HhLv2Application.java
@@ -0,0 +1,13 @@
+package org.sparta.hh_lv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhLv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhLv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/hh_lv2/HhLv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.hh_lv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhLv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
